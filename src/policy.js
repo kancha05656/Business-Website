50b159334debb8003163e00c55b29834439d29d4
AlaminAB <alaminislam907@gmail.com>
2020-05-08 01:15:55 +0600

    testimonial edit

--- src/policy.js
+++ src/policy.js
@@ -17,83 +17,13 @@ import contact_banner from './img/contact-bg.png'
 
 
 
- <section className="contact-us-section-wrap">
-    <div className="container">
-      <div className="contact-us-wrap-box">
 
+<section class="policy">
+  <div class="container">
+    
+  </div>
+</section>
 
-        <div className="row justify-content-center text-center">
-          <div className="col-lg-8 col-xl-6">
-
-            <div className="section-title">
-              <h2>Contact <span>Us</span></h2>
-              <p>Fill out the form with your information and project description.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-
-          <div className="col-lg-8">
-            <form action="#" className="contact-form">
-              <div className="row">
-                <div className="col-md-6 form-group">
-                  <label htmlFor="name" className="d-none">Your Name</label>
-                  <input type="text" className="form-control" placeholder="Full Name" />
-                </div>
-                <div className="col-md-6 form-group">
-                  <label htmlFor="email" className="d-none">Your Email</label>
-                  <input type="email" className="form-control" placeholder="Email" />
-                </div>
-                <div className="col-12 form-group">
-                  <label htmlFor="subject" className="d-none">Subject</label>
-                  <input type="email" className="form-control" placeholder="Subject" />
-                </div>
-                <div className="col-12 form-group">
-                  <label htmlFor="message" className="d-none">Message</label>
-                  <textarea name="message" className="form-control" id="message" placeholder="Describe your project."></textarea>
-                </div>
-                <div className="col-12 form-btn">
-                  <button type="submit" className="btn modern-btn">Send</button>
-                </div>
-              </div>
-            </form>
-          </div>
-
-
-          <div className="col-lg-4">
-            <div className="contact-location-box">
-              <h3>Our Address</h3>
-              <p>Other Contact informations</p>
-
-
-
-              <div className="single-location-widget">
-                <h4><i className="fas fa-envelope"></i> Email Address</h4>
-                <p>webapprevolutions@gmail.com</p>
-              </div>
-
-              <div className="single-location-widget">
-                <h4><i className="fas fa-phone"></i> Phone Number</h4>
-                <p>(616) 953-6133</p>
-              </div>
-
-              <div className="single-location-widget">
-                <h4><i className="fas fa-map"></i>Location</h4>
-                <p>Lansing, Michigan 48911</p>
-              </div>
-
-
-
-            </div>
-          </div>
-
-        </div>
-
-
-      </div>
-    </div>
-  </section>
 
 
 
